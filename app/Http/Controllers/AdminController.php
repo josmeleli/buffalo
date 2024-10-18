991dfedfe4c8e53b43035aee1b121394967019f8
codecraftdev8@gmail.com <angeliglesias@gmail.com>
2024-10-18 13:59:41 -0500

    avances panel admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\insumo;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -11,6 +14,17 @@ class AdminController extends Controller
     }
 
     public function index(){
-        return view('Admin.index');
+        $totalInsumo = insumo::all()->count();
+        $ultimoInsumo = Insumo::latest()->first();
+        $fechaUltimoInsumo = $ultimoInsumo ? $ultimoInsumo->created_at->format('d/m/Y') : 'No hay registros';
+        $anioActual = Carbon::now()->year;
+        $insumoBajo = Insumo::where('stock', '<', 5)->count();
+
+        $insumosPorMes = Insumo::select(
+            DB::raw('MONTH(created_at) as mes'),
+            DB::raw('COUNT(*) as cantidad')
+        )->groupBy('mes')->get();
+
+        return view('Admin.index', compact('totalInsumo', 'fechaUltimoInsumo', 'anioActual', 'insumoBajo','insumosPorMes'));
     }
 }
